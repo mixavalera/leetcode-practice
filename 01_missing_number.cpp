fe7b522a9ccc9b09216eb508b25c95f91d703045
mixavalera <mixavalera031@gmail.com>
2026-06-14 21:36:13 +0300

    Update 01_missing_number.cpp

--- 01_missing_number.cpp
+++ 01_missing_number.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
 
+        int fullSum = n * (n + 1) / 2;
+        int currentSum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            currentSum += nums[i];
+        }
+
+        return fullSum - currentSum;
+    }
+};
